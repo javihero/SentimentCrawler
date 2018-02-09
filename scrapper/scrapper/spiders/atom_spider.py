47958d6071a1b615c9c80f161d431b5d27924116
javislaterlp <javier.hernandez@edosoft.es>
2018-02-09 12:37:17 +0000

    Atom spider

---
+++ scrapper/scrapper/spiders/atom_spider.py
@@ -0,0 +1,23 @@
+from scrapy.spiders import XMLFeedSpider
+from scrapper.helpers import sanitize_content
+
+
+class BSpider(XMLFeedSpider):
+    name = "atom"
+    namespaces = [('atom', 'http://www.w3.org/2005/Atom')]
+    iterator = 'xml'
+    itertag = 'atom:entry'
+
+    def parse_node(self, response, node):
+        self.logger.info('Hi, this is a <%s> node!: %s',
+                         self.itertag, ''.join(node.extract()))
+
+        entry = {}
+        entry['title'] = node.xpath('atom:title/text()',).extract_first()
+        entry['link'] = node.xpath('atom:link/text()').extract_first()
+        entry['updated'] = node.xpath('atom:updated/text()').extract_first()
+
+        content = node.xpath('atom:content/text()').extract_first()
+        entry['content'] = sanitize_content(content)
+
+        return entry
