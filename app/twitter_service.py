52ef6bcd81fd1bbf21ca92736afa7d2fe0b28fd7
javislaterlp <javier.hernandez@edosoft.es>
2018-02-14 13:34:03 +0000

    TwitterService simplified

--- app/twitter_service.py
+++ app/twitter_service.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Twitter API calls are made from here
-"""
 
 from twitter_config import API_KEY, API_SECRET, OAUTH_KEY, OAUTH_SECRET
 import twitter
 
 
 class TwitterService():
+    """
+    Twitter API calls are made from here
+    """
+
     api = twitter.Api(consumer_key=API_KEY,
                       consumer_secret=API_SECRET,
                       access_token_key=OAUTH_KEY,
@@ -18,15 +19,5 @@ class TwitterService():
         return self.api.VerifyCredentials()
 
     def get_tweets_from(self, term):
-        info = []
-
-        # result = self.api.GetSearch(
-        # raw_query="q=from%3A" + term)
-
-        result = self.api.GetUserTimeline(
+        return self.api.GetUserTimeline(
             screen_name=term, count=100, exclude_replies=True)
-
-        for status in result:
-            info.append(status)
-
-        return info
