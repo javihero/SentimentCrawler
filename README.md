03d080a20fc418505f8f37fdc28af3e8c91abe30
javislaterlp <javier.hernandez@edosoft.es>
2018-02-15 11:02:40 +0000

    Solved merge of  gcs logic from Deyan, edited README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Sentiment crawler
+#Sentiment crawler
+
+##App
+  This folder contains the web page that is deployed in App Engine, requires Python 2.7
+
+  * 'dev_appserver app.yaml' to serve in localhost
+  * 'gcloud app deploy' to submit changes to App Engine
+
+##Scrapper
+  This folder contains the Scrapy project (crawlers), requires Python 3
+
+  * 'scrapyrt' to serve an API on localhost:9080
+  * URL format is -> 'http://localhost:9080/crawl.json?spider_name=' + spider + '&url=' + url 
--- app/main.py
+++ app/main.py
@@ -23,6 +23,11 @@ import json
 import jinja2
 import webapp2
 
+import csv
+
+# Import the Google Cloud Storage Library
+import cloudstorage as gcs
+
 from twitter_service import TwitterService
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -30,6 +35,15 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 # [END imports]
+
+
+# [START retries]
+my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
+                                          max_delay=5.0,
+                                          backoff_factor=2,
+                                          max_retry_period=15)
+gcs.set_default_retry_params(my_default_retry_params)
+# [END retries]
 
 
 # [START main_page]
@@ -126,12 +140,116 @@ class RequestTwitter(webapp2.RequestHandler):
 # [END twitter]
 
 
+# [START read brands, media and kind]
+class CloudStorage(webapp2.RequestHandler):
+
+    buck_name = 'urlbucket'
+    brands_file_name = 'brands.csv'
+    media_file_name = 'medios.csv'
+
+    def get(self):
+
+        bucket_name = os.environ.get('BUCKET_NAME', self.buck_name)
+
+        kinds = ['website', 'rss', 'twitter']
+
+        bucket = '/' + bucket_name
+        brands_filename = bucket + '/' + self.brands_file_name
+        media_filename = bucket + '/' + self.media_file_name
+        self.tmp_filenames_to_clean_up = []
+
+        try:
+            brands = self.read_file(brands_filename)
+            medios = self.read_file(media_filename)
+        except Exception, e:
+            self.response.write(e)
+
+        template_values = {
+            'brands': brands,
+            'medios': medios,
+            'tipos': kinds
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('buckets.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+
+        bucket_name = os.environ.get('BUCKET_NAME', self.buck_name)
+
+        kinds = ['website', 'rss', 'twitter']
+
+        bucket = '/' + bucket_name
+        brands_filename = bucket + '/' + self.brands_file_name
+        media_filename = bucket + '/' + self.media_file_name
+        self.tmp_filenames_to_clean_up = []
+
+        try:
+            brands = self.read_file(brands_filename)
+            medios = self.read_file(media_filename)
+        except Exception, e:
+            self.response.write(e)
+
+        brand_selected = self.request.get('brand')
+        medio_selected = self.request.get('medio')
+        kind_selected = self.request.get('kind')
+
+        # ----- Take URLs ----- #
+
+        url_file_name = ''
+
+        if (kind_selected == 'website'):
+            url_file_name = 'website.csv'
+        elif (kind_selected == 'rss'):
+            url_file_name = 'rss.csv'
+        else:
+            url_file_name = 'twitter.csv'
+
+        url_filename = bucket + '/' + url_file_name
+
+        try:
+            urls = self.read_urls_file(url_filename, medio_selected)
+        except Exception, e:
+            self.response.write(e)
+
+        template_values = {
+            'brands': brands,
+            'medios': medios,
+            'tipos': kinds,
+            'brand_selected': brand_selected,
+            'medio_selected': medio_selected,
+            'kind_selected': kind_selected,
+            'urls': urls
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('buckets.html')
+        self.response.write(template.render(template_values))
+
+    def read_file(self, filename):
+        gcs_file = gcs.open(filename)
+        readCSV = csv.reader(gcs_file, delimiter=',')
+        for row in readCSV:
+            yield str(row[0]).decode('utf-8')
+        gcs_file.close()
+
+    def read_urls_file(self, filename, medio_selected):
+        gcs_file = gcs.open(filename)
+        readCSV = csv.reader(gcs_file, delimiter=',')
+        for row in readCSV:
+            if (medio_selected == str(row[1]).decode('utf-8')):
+                yield str(row[0]).decode('utf-8')
+        gcs_file.close()
+
+# [END brands]
+
+
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/rss', ScrapyRss),
     ('/atom', ScrapyAtom),
     ('/web', ScrapyWeb),
-    ('/twitter', RequestTwitter)
+    ('/twitter', RequestTwitter),
+    ('/gcs', CloudStorage)
 ], debug=True)
 # [END app]
