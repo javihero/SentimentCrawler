a1d6393d3804e03944961ea1430128ba7209648a
javislaterlp <javier.hernandez@edosoft.es>
2018-02-22 11:42:32 +0000

    urllib2 -> urlfetch (standard appengine lib)

--- app/helpers.py
+++ app/helpers.py
@@ -1,17 +1,7 @@
-import urllib2
-
 import json
-import cloudstorage as gcs
-from google.appengine.api import taskqueue
+from google.appengine.api import taskqueue, urlfetch
 
-#[START retries]
-my_default_retry_params = gcs.RetryParams(initial_delay=0.2,
-                                          max_delay=5.0,
-                                          backoff_factor=2,
-                                          max_retry_period=15)
-gcs.set_default_retry_params(my_default_retry_params)
-#[END retries]
-
+urlfetch.set_default_fetch_deadline(60)
 
 
 def sanitize_url(old_url):
@@ -23,24 +13,10 @@ def sanitize_url(old_url):
 
 
 def request_scrapy(api_url):
-    content = urllib2.urlopen(api_url).read()
+    content = urlfetch.fetch(api_url).content
     json_content = json.loads(content)
     return json_content['items']
 
-"""
-def send_to_gstorage(self, filename, content):
-    self.tmp_filenames_to_clean_up = []
-    write_retry_params = gcs.RetryParams(backoff_factor=1.1)
-    gcs_file = gcs.open(filename,
-                        'w',
-                        content_type='text/plain',
-                        options={'x-goog-meta-foo': 'foo',
-                                 'x-goog-meta-bar': 'bar'},
-                        retry_params=write_retry_params)
-    gcs_file.write(str(content))
-    gcs_file.close()
-    self.tmp_filenames_to_clean_up.append(filename)
-"""   
 
 def send_to_bq_task(item):
-        taskqueue.add(url='/send_bq', target='worker', params=item)
+    taskqueue.add(url='/send_bq', target='worker', params=item)
