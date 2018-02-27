ffcd6533b36e4f290095943919c75badd1c1985b
javislaterlp <javier.hernandez@edosoft.es>
2018-02-27 09:42:41 +0000

    Moved BQ logic to bq service

--- app/bigquery_service.py
+++ app/bigquery_service.py
@@ -1,36 +1,59 @@
 # Import the Google Cloud BigQuery Library
 from google.cloud import bigquery
-from google.cloud.bigquery import Dataset
-
-import os
-import logging
-logging.basicConfig(level=logging.DEBUG)
+from google.appengine.api import taskqueue
 
 from io import StringIO
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+def send_to_bq_task(bq_client, table_ref, job_config, item):
+    taskqueue.add(url='/send_bq', target='worker', params={
+                  'bq_client': bq_client, 'table_ref': table_ref, 'job_config': job_config, 'item': item})
+
+
+def get_dict_datasets_tables():
+    bq_client = bigquery.Client()
+    datasets = bq_client.list_datasets()  # API request(s)
+    dict_datasets_tables = {}
+    for single_dataset in datasets:
+        dataset = bigquery.Dataset(
+            bq_client.dataset(single_dataset.dataset_id))
+        dataset_tables = bq_client.list_dataset_tables(dataset)
+        dict_datasets_tables[single_dataset.dataset_id] = []
+        for dataset_table in dataset_tables:
+            dict_datasets_tables[single_dataset.dataset_id].append(
+                dataset_table.table_id)
+
+    return dict_datasets_tables
+
 
 def send_scrapper_result_to_bigquery(data):
-    
+
     dataset_name = 'sentimentcrawlerdataset'
     table_id = 'scrapper_table'
-        
+
     bigquery_client = bigquery.Client()
     dataset_ref = bigquery_client.dataset(dataset_name)
     table_ref = dataset_ref.table(table_id)
-    
+
     job_config = bigquery.LoadJobConfig()
     job_config.source_format = 'NEWLINE_DELIMITED_JSON'
     job_config.autodetect = True
 
-    #load_job = bigquery_client.load_table_from_uri('https://storage.cloud.google.com' + source_file_name, table_ref, job_config=job_config)  # API request
-    load_job = bigquery_client.load_table_from_file(StringIO(unicode(data)), table_ref, job_config=job_config) # API request
+    # load_job = bigquery_client.load_table_from_uri('https://storage.cloud.google.com' + source_file_name, table_ref, job_config=job_config)  # API request
+    load_job = bigquery_client.load_table_from_file(
+        StringIO(unicode(data)), table_ref, job_config=job_config)  # API request
 
     load_job.result()  # Waits for table load to complete.
+
 
 def get_bq_scrapertext(dataset, table):
     bq_client = bigquery.Client()
     table_ref = bq_client.dataset(dataset).table(table)
-    iterator = bq_client.list_rows(table_ref, selected_fields=[bigquery.SchemaField('TEXT', 'STRING')])
+    iterator = bq_client.list_rows(table_ref, selected_fields=[
+                                   bigquery.SchemaField('TEXT', 'STRING')])
     rows = list(iterator)
 
     return rows
@@ -40,13 +63,14 @@ def save_sentiment_result_to_bq(dataset_selected, table_selected, sentiment_resu
     bq_client = bigquery.Client()
     dataset_ref = bq_client.dataset(dataset_selected)
 
-     # [START create_table for sentiment result]
+    # [START create_table for sentiment result]
     SCHEMA = [
         bigquery.SchemaField('TEXT', 'STRING'),
         bigquery.SchemaField('SCORE', 'STRING'),
         bigquery.SchemaField('MAGNITUDE', 'STRING'),
     ]
-    table_ref = dataset_ref.table(str(table_selected) + '_' + 'sentiment_result')
+    table_ref = dataset_ref.table(
+        str(table_selected) + '_' + 'sentiment_result')
     table = bigquery.Table(table_ref, schema=SCHEMA)
     table = bq_client.create_table(table)      # API request
 
@@ -56,14 +80,9 @@ def save_sentiment_result_to_bq(dataset_selected, table_selected, sentiment_resu
     row_to_insert_text = [
         (u'Phred Phlyntstone', '21', '45')
     ]
-    
+
     bq_client.insert_rows(table, row_to_insert_text)  # API request
 
-    #logging.info('ERRORS: ' + str(errors))
+    # logging.info('ERRORS: ' + str(errors))
 
     # [END table_insert_rows]
-
-
-
-
-
--- app/helpers.py
+++ app/helpers.py
@@ -1,9 +1,6 @@
 import json
-from google.appengine.api import taskqueue, urlfetch
+from google.appengine.api import urlfetch
 
-# Import the Google Cloud BigQuery Library
-from google.cloud import bigquery
-from google.cloud.bigquery import Dataset
 
 def sanitize_url(old_url):
     if not old_url.startswith(('http://', 'https://')):
@@ -18,20 +15,3 @@ def request_scrapy(api_url):
     content = urlfetch.fetch(api_url).content
     json_content = json.loads(content)
     return json_content['items']
-
-
-def send_to_bq_task(bq_client, table_ref, job_config, item):
-        taskqueue.add(url='/send_bq', target='worker', params={'bq_client': bq_client, 'table_ref': table_ref, 'job_config': job_config, 'item': item})
-
-def get_dict_datasets_tables():
-    bq_client = bigquery.Client()
-    datasets = bq_client.list_datasets() # API request(s)
-    dict_datasets_tables = {}
-    for single_dataset in datasets:
-        dataset = bigquery.Dataset(bq_client.dataset(single_dataset.dataset_id))
-        dataset_tables = bq_client.list_dataset_tables(dataset)
-        dict_datasets_tables[single_dataset.dataset_id] = []
-        for dataset_table in dataset_tables:
-            dict_datasets_tables[single_dataset.dataset_id].append(dataset_table.table_id)
-        
-    return dict_datasets_tables
